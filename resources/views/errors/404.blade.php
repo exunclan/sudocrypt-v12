8f804e88b24e52dea83df0be553c133b576b8ee6
Mehul Srivastava <84122163+mehul-srivastava@users.noreply.github.com>
2022-11-07 00:05:49 +0530

    change: default 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sudocrypt v12.0</title>
+</head>
+<body bgcolor="#000">
+    <img src="/img/mario-404.gif" />
+</body>
+</html>
