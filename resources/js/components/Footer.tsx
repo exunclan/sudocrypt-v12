e85a7a3857876314604bb92d304795d8644b54de
GarvJain03 <garvj03@gmail.com>
2022-11-07 00:58:20 +0530

    fix: 2021-22 to 2022

--- resources/js/components/Footer.tsx
+++ resources/js/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-center py-4 text-gray-500 flex-col text-sm sm:text-xs text-center px-2">
+    <footer className="flex items-center font-mono justify-center py-4 text-gray-500 flex-col text-sm sm:text-xs text-center px-2">
       <div className="flex mb-3 text-white">
         <a
           href="https://www.facebook.com/sudocrypt/"
@@ -39,10 +39,7 @@ export default function Footer() {
           target="_blank"
           rel="noreferrer"
         >
-          <svg
-            style={{ width: "24px", height: "24px" }}
-            viewBox="0 0 24 24"
-          >
+          <svg style={{ width: "24px", height: "24px" }} viewBox="0 0 24 24">
             <path
               fill="currentColor"
               d="M19 3A2 2 0 0 1 21 5V19A2 2 0 0 1 19 21H5A2 2 0 0 1 3 19V5A2 2 0 0 1 5 3H19M18.5 18.5V13.2A3.26 3.26 0 0 0 15.24 9.94C14.39 9.94 13.4 10.46 12.92 11.24V10.13H10.13V18.5H12.92V13.57C12.92 12.8 13.54 12.17 14.31 12.17A1.4 1.4 0 0 1 15.71 13.57V18.5H18.5M6.88 8.56A1.68 1.68 0 0 0 8.56 6.88C8.56 5.95 7.81 5.19 6.88 5.19A1.69 1.69 0 0 0 5.19 6.88C5.19 7.81 5.95 8.56 6.88 8.56M8.27 18.5V10.13H5.5V18.5H8.27Z"
@@ -61,9 +58,7 @@ export default function Footer() {
             Delhi Public School, R.K. Puram
           </a>
         </div>
-        <div>
-          &copy; 2021-22 Exun Clan and DPS RK Puram, New Delhi, India
-        </div>
+        <div>&copy; 2022 Exun Clan and DPS RK Puram, New Delhi, India</div>
       </div>
     </footer>
   );
